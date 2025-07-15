363e7e806cc0bfff0c31a1384730206dd1482648
J3ffers0n02 <mjelizaga13@gmail.com>
2025-07-15 15:49:04 +0800

    Add files via upload

---
+++ portfoliio-site/sidebar_elements/sidebar_active.js
@@ -0,0 +1,11 @@
+// sidebar_active.js
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('.sidebar ul li a');
+  const currentUrl = window.location.href;
+
+  links.forEach(link => {
+    if (link.href === currentUrl) {
+      link.classList.add('active');
+    }
+  });
+});
